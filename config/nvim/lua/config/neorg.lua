b3a898310e716559a51d1508913481434bf911f3
Oleg Karfich <oleg.karfich@wago.com>
2023-04-21 16:02:13 +0200

    config: nvim: neorg.lua: fix missconfiguration
    Signed-off-by: Oleg Karfich <oleg.karfich@wago.com>

--- config/nvim/lua/config/neorg.lua
+++ config/nvim/lua/config/neorg.lua
@@ -4,13 +4,13 @@ function M.setup()
   require("neorg").setup {
     load = {
       ["core.defaults"] = {},
-      ["core.norg.concealer"] = {},
-      ["core.norg.completion"] = {
+      ["core.concealer"] = {},
+      ["core.completion"] = {
         config = {
           engine = "nvim-cmp",
         },
       },
-      ["core.norg.dirman"] = {
+      ["core.dirman"] = {
         config = {
           workspaces = {
             my_workspace = "~/workspace/dev/notes",
