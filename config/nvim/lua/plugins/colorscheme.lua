bcbf2a3045b199f0c21c4a170686ed3288e154c8
Oleg Karfich <oleg.karfich@wago.com>
2025-02-20 17:05:03 +0100

    config: nvim: lua: plugins: colorscheme: configure ayu theme
    Signed-off-by: Oleg Karfich <oleg.karfich@wago.com>

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,28 @@
 return {
-	-- add neovim ayu dark plugin
-	{ "Shatur/neovim-ayu" },
-
-	-- Configure LazyVim to load ayu colorscheme
+	{
+		"Shatur/neovim-ayu",
+		config = function()
+			require("ayu").setup({
+				mirage = false, -- Disable the mirage variant to use ayu-dark
+				overrides = function()
+					local colors = require("ayu.colors")
+					return {
+						LineNr = { fg = colors.comment },
+						CursorLineNr = { fg = "#FFD700", bold = true },
+						NonText = { fg = colors.comment },
+						WinSeparator = { fg = colors.comment },
+						VertSplit = { fg = colors.comment },
+						SnacksIndent = { fg = colors.guide_normal },
+						Whitespace = { fg = colors.guide_normal },
+					}
+				end,
+			})
+		end,
+	},
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "ayu",
+			colorscheme = "ayu-dark",
 		},
 	},
 }
